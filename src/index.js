22d402653cd71f2bc2683b0cf2659687d84823a3
bssoleil <sadia.bentahar59@gmail.com>
2019-05-20 12:20:23 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+            <h1 className="site-heading">Hello, React!</h1>
+            </div>
+        )
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
